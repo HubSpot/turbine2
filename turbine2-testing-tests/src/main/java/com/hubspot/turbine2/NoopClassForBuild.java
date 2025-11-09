873730237bbee455d61942f2250c2c9361aac086
Michael Axiak <maxiak@hubspot.com>
2025-11-09 09:36:00 -0500

    Add a noop class to make the build pass

---
+++ turbine2-testing-tests/src/main/java/com/hubspot/turbine2/NoopClassForBuild.java
@@ -0,0 +1,8 @@
+package com.hubspot.turbine2;
+
+/**
+ * This class only exists to make it easy for the maven
+ * logic enforcing regular builds to provide java class
+ * files to work.
+ */
+public class NoopClassForBuild {}
